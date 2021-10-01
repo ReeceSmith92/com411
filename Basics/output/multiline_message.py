160f2bee213e14e7b40a2a4f029202757f34a54e
Reece Smith <5smitr09@solent.ac.uk>
2021-10-01 15:10:37 +0100

    new line of coe

--- Basics/output/multiline_message.py
+++ Basics/output/multiline_message.py
@@ -2,3 +2,7 @@
 
 print("System Failure imminent!")
 print("Sytem reboot has been initiated...")
+
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
